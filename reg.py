de1c7b2ee4d4bf8880e3a7fb6483a8cd0521a961
hacker41601 <monynichkiem@gmail.com>
2022-02-21 15:52:37 -0500

    Create python file and import needed libraries as well as add in author, data, and description

---
+++ reg.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import math
+
+#Author: Monynich Kiem
+#Date: Feb. 21. 2022
+#Desc: Linear regression and polynomial expansion program using csv data being read in
+
