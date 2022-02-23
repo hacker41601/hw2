c7caf96733a191532a2f9f530e16cd5aecf10588
hacker41601 <monynichkiem@gmail.com>
2022-02-23 07:28:46 -0500

    optimizing and automating

--- reg.py
+++ reg.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 #Author: Monynich Kiem
 #Date: Feb. 21. 2022
 #Desc: Linear regression and polynomial expansion program using csv data being read in
+#using SGD since it's allegedly faster
 '''
 def hypothesis(theta, dataset_x):
     return theta*dataset_x
@@ -40,8 +41,8 @@ cost_iter, j, theta = gradient(x, y, theta, alpha, epoch)
 
 #hyperparameters are the alpha and epoch
 alpha = 0.001 #dr harrison said smaller numbers are better
-max_epoch = 10 #epochs are just the number of iterations
-num_features = 11 #because there are 11 in this specific dataset
+max_epoch = 42 #epochs are just the number of iterations, chose 42 b/c nerd stuff
+#num_features = 11 #because there are 11 in this specific dataset
 weights = []
 #for x in range(num_features + 1): #+1 is to account for the size being increased since the first input is always 1
 #    weights.append(random()) #ranadomize float between 0 to 1
@@ -61,65 +62,73 @@ y = wine.iloc[:,12] #output variable
 '''
 #pt 1--------------------------------------------------------------------------
 wine = np.loadtxt('winequality-red.csv', delimiter = ',', skiprows = 1)
-epoch = 1
+curr_epoch = 0
+#num_features = len(wine[0,:])-1 #does not include the output/target class label
+#print(num_features)
 #print(wine[:,0]) #prints first column
 #print(wine[0,:]) #prints first row
 #print(type(wine))
 #https://www.kite.com/python/answers/how-to-iterate-through-columns-of-a-numpy-array-in-python
 def normalize(dataset):
     columns = dataset.shape[1]
-    print(columns-1)
+    #print(columns-1)
     for i in range(columns-1):
         dataset[:,i] = dataset[:,i]/np.max(dataset[:,i])
 normalize(wine)
-print(wine)
+#print(wine)
 
-while epoch <= max_epoch:
-    ex = 0
-    for i in wine:
-        entry = np.array(wine[ex])
-        #print(entry)
-        feat = entry[:-1] #only the features doesnt include the quality rating
-        #print(feat)
-        input = np.insert(feat, 0, 1.0) #x0 is always 1
-        #print(input)
-        #print(np.shape(weights))
-        #print(np.shape(input))
-        #print(type(input))
-        weights = []
-        for j in range(num_features + 1): #+1 is to account for the size being increased since the first input is always 1
-            weights.append(random()) #ranadomize float between 0 to 1
-        weights = np.array(weights)
-        hypothesis = np.dot((np.transpose(weights)), input) #scalar
-        #print(hypothesis)
-        #print(type(hypothesis))
+def sgd(dataset, max_epoch, alpha):
+    curr_epoch = 0
+    while curr_epoch <= max_epoch:
+        ex = 0
+        for i in wine:
+            entry = np.array(dataset[ex])
+            #print(entry)
+            inputs = entry[:-1] #only the features doesnt include the quality rating
+            #print(inputs)
+            input = np.insert(inputs, 0, 1.0) #x0 is always 1
+            #sgd uses randomized weights and handles a vector at a time
+            #print(input)
+            #print(np.shape(weights))
+            #print(np.shape(input))
+            #print(type(input))
+            
+            weights = []
+            for j in range(len(wine[0,:])): #+1 is to account for the size being increased since the first input is always 1 which i inserted
+                weights.append(random()) #ranadomize float between 0 to 1
+            weights = np.array(weights)
+            hypothesis = np.dot((np.transpose(weights)), input) #scalar
+            #print(hypothesis)
+            #print(type(hypothesis))
+            
+            pred = dataset[ex][(len(dataset[0,:])-1)]
+            #print(pred)
         
-        gt = wine[ex][(num_features-1)]
-        #print(gt)
+            raw_err = hypothesis - pred #scalar
+            #print(raw_err)
         
-        raw_err = hypothesis - gt #scalar
-        #print(raw_err)
+            gradient = raw_err * input #1 x n
+            #update weights:
         
-        gradient = raw_err * input #1 x n
-        #update weights:
-        
-        temp = weights
-        feat = 0
-        for y in weights:
-            weights[feat] = temp[feat] - alpha * gradient[feat]
-            feat += 1
-        mse = np.dot(np.transpose(weights), input)
-        mse = mse - gt
-        mse = abs(mse)
-        MSE.append(mse)
-        ex += 1
-    avgSE = sum(MSE)/ len(MSE)
+            temp = weights
+            feat = 0
+            for y in weights:
+                weights[feat] = temp[feat] - alpha * gradient[feat]
+                feat += 1
+            mse = np.dot(np.transpose(weights), input)
+            mse = mse - pred
+            mse = abs(mse)
+            MSE.append(mse)
+            ex += 1
+        avgSE = sum(MSE)/ len(MSE)
     
-    if epoch % 10 == 0:
-        print(avgSE)
-    epoch += 1
+        if curr_epoch % 7 == 0:
+            print(avgSE)
+        curr_epoch += 1
 
-print(weights)
+    print(weights)
+
+sgd(wine, max_epoch, alpha)
 
 '''
 x1 = [i[0] for i in wine]
