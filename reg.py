b25aaf87a3a8ea61ca814e52bfc74da64edd4732
hacker41601 <monynichkiem@gmail.com>
2022-02-26 13:10:47 -0500

    sgd on new dataset is not working for some reason

--- reg.py
+++ reg.py
@@ -116,36 +116,46 @@ synth2.columns = ['x0', 'input', 'quality']
 #print(synth2)
 
 def basis_exp(dataset, order): #the orders or 2, 3, and 5
+#expand dataset to their respective orders
     ex = 0
     exp_ind = 1
+    newcol = 1
     for x in range(order - 1):
         exp_ind+=1
         #dataset[2 + exp_ind] = 0
-        dataset.insert(exp_ind, 1, 0)
+        newcol += 1
+        dataset.insert(exp_ind, newcol, 0)
         #use pandas concat instead and see if it works
     for m in dataset:
         ind = 1
-        og = dataset.iloc[ex][ind]
+        og = dataset.iloc[ex, ind]
         for x in range(order - 1):
             ind+=1
-            cast = float(og) ** (ind + 1)
-            dataset.iloc[ex][ind] = cast
+            ordered = og ** (ind + 1)
+            dataset.iloc[ex,ind] = ordered
         ex += 1
         
 sgd(synth1, max_epoch, alpha)
 sgd(synth2, max_epoch, alpha)
 
-print(synth1.iloc[0][1])
+#print(synth1.iloc[0][1])
 
 #lines 139 - 147 work
 for m in synth1:
     ex = 0
-    ind = 0
-    og = synth1.iloc[ex][ind+1]
-    print(og)
+    ind = 1
+    og = synth1.iloc[ex][ind + 1]
+    #print(og)
 
 #inserts into 2nd column, named 1, filled with 0s
-synth1.insert(2, 1, 0)
+#synth1.insert(2, 1, 0)
+basis_exp(synth1, 5)
+#og = synth1.iloc[0,1]
+#og = og ** 2
+#print(og)
+#synth1.iloc[0,2] = og
+#print(synth1.iloc[0,2])
+#for some reason it all goes to zeroes at row 7
 print(synth1)
-#basis_exp(synth1, 5)
+#sgd(synth1, max_epoch, alpha)
 
